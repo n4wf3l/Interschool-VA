7b57acfab459c7e0b9c712813c16ccf8b8d21323
jackthyssens <jack.thyssens@student.ehb.be>
2023-12-13 09:35:21 +0100

    push voornamm toevoegen aan migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('userID');
+            $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
             $table->boolean('admin')->default(false);
