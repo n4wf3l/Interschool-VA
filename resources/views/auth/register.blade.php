5a32508f4cae917ff3b748027b344125f8454664
Nawfel Ajari <ajari.nawfel@hotmail.com>
2023-12-16 17:19:56 +0100

    hover transition

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,14 +56,18 @@
 <main class="bg-white flex-1 pt-20">
     <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
     <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-        <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-            <span class="me-2">1</span>
+        <span class="hover:text-teal-500 duration-500 flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            <span class="me-2">            <button type="button" onclick="window.location.href='{{ url('/') }}'" class="text-white bg-teal-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0l4-4M1 5l4 4"/>
+    </svg>
+</button></span>
             Home <span class="hidden sm:inline-flex sm:ms-2">inschrijving</span>
         </span>
     </li>
 
     <li class="flex md:w-full items-center text-red-500 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-        <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+        <span class="hover:text-teal-500 duration-500 flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
             <svg class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
@@ -71,7 +75,7 @@
         </span>
     </li>
 
-    <li class="flex items-center">
+    <li class="flex items-center hover:text-teal-500 duration-500">
     <span class="me-2">3</span>
             Kies <span class="hidden sm:inline-flex sm:ms-2">team</span>
     </li>
@@ -98,11 +102,7 @@
                 <!-- Titre -->
                 <h2 class="text-8xl font-bold mt-2 mb-4 duration-500 pl-5 pb-15">VUL DE FORMULIER</h2>
             </div>
-            <button type="button" onclick="window.location.href='{{ route('registerteams') }}'" class="text-white bg-teal-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0l4-4M1 5l4 4"/>
-    </svg>
-</button>
+
             <form class="max-w-sm mx-auto pb-8" method="POST" action="{{ route('register') }}">
                 @csrf
 
@@ -161,8 +161,8 @@
                     </div>
                 </div>
 
-                <div class=" md:flex-row justify-center mb-6">
-                    <a class="pr-6 text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                <div class=" md:flex-row justify-center mb-6 text-center">
+                    <a class="pr-6 text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-5" href="{{ route('login') }}">
                         {{ __('Al ingeschreven?') }}
                     </a>
 
