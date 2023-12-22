8be53169fcbdaee08eeceb0de14fbe2c30fd9be3
kvsj123 <kristian.vasiaj@student.ehb.be>
2023-12-22 22:00:45 +0100

    refactor(archived ranking): show max 10 teams

--- resources/views/rankings/archived.blade.php
+++ resources/views/rankings/archived.blade.php
@@ -3,7 +3,7 @@
         <div class="form-group">
             <label for="year">Select Year:</label>
             <select name="year" id="year" class="form-control" onchange="this.form.submit()">
-                <option value="">Choose a Year</option>
+                <option value="">GOAT</option>
                 @foreach ($years as $year)
                 <option value="{{ $year }}" {{ $selectedYear==$year ? 'selected' : '' }}>{{ $year }}</option>
                 @endforeach
@@ -15,7 +15,7 @@
     @if ($rankings->isNotEmpty())
     <h2>Rankings for {{ $selectedYear ?: 'All Years' }}</h2>
     <ul>
-        @foreach ($rankings as $ranking)
+        @foreach ($rankings->take(10) as $ranking)
         <li>{{ $ranking->teamname }} - {{ $ranking->points }} Points</li>
         @endforeach
     </ul>
