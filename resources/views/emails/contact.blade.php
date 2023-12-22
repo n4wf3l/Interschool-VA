36e170590cce7ff443cd68d11b54d38270ccfe66
kvsj123 <kristian.vasiaj@student.ehb.be>
2023-12-22 23:16:52 +0100

    chore(email Content): modify content of the emails body

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,5 +1,7 @@
 {{-- resources/views/emails/contact.blade.php --}}
 
+<h1>Contacteer Ons Bericht</h1>
+<br>
 <p>Name: {{ $data['naam'] }}</p>
 <p>Email: {{ $data['email'] }}</p>
 <p>Message: {{ $data['bericht'] }}</p>
--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -1,3 +1,20 @@
-{{-- resources/views/emails/notification.blade.php --}}
+<!DOCTYPE html>
+<html>
 
-<p>Message: {{ $messageContent}}</p>
+<head>
+    <title>Algemene communicatie</title>
+</head>
+
+<body>
+    <h1>Communicatie voor alle teams</h1>
+    <p>Beste Teams,</p>
+
+    <p>{{ $messageContent }}</p>
+
+    <p>Bedankt voor jullie snelle reactie.</p>
+
+    <p>Met vriendelijke groet,</p>
+    <p>Interschool Voetbal Applicatie</p>
+</body>
+
+</html>
