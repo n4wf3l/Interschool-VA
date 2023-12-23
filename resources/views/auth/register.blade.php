e0c31026969e48bf701c0850aaa9ea3289fab71e
Nawfel Ajari <ajari.nawfel@hotmail.com>
2023-12-23 15:53:40 +0100

    refactor responsiveness of ranking page : title, redirection button to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,7 +40,8 @@
             @endauth
           </li>
           <li class="mx-4 my-0 md:my-0 bg-red">
-            <a href="#" class="text x1 hover:text-teal-500 duration-500" style="background-color: red;">OVER ONS</a>
+            <a href="{{ url('/about') }}" class="text x1 hover:text-teal-500 duration-500"
+              style="background-color: red;">OVER ONS</a>
           </li>
           <li class="mx-4 my-0 md:my-0 bg-red">
             <a href="{{ url('/calendars') }}" class="text x1 hover:text-teal-500 duration-500"
