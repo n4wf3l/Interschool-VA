78c0a264e903f5d848d7a54ba1b899004827c51b
Nawfel Ajari <ajari.nawfel@hotmail.com>
2023-12-23 16:00:50 +0100

    refactor: padding left in input textareas

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -16,13 +16,13 @@
         <div>
             <x-input-label for="update_password_current_password" :value="__('Huidig wachtwoord')" />
             <x-text-input id="update_password_current_password" name="current_password" type="password"
-                class="mt-1 block w-full" autocomplete="current-password" />
+                class="mt-1 block w-full pl-3" autocomplete="current-password" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="update_password_password" :value="__('Nieuwe wachtwoord')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full"
+            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full pl-3"
                 autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
@@ -30,7 +30,7 @@
         <div>
             <x-input-label for="update_password_password_confirmation" :value="__('Bevestig wachtwoord')" />
             <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password"
-                class="mt-1 block w-full" autocomplete="new-password" />
+                class="mt-1 block w-full pl-3" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
